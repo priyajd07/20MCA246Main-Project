89744b241a0d13a59ae0b89c647723041d1b743d
haq9966 <haq9966@gmail.com>
2022-05-09 10:36:32 +0530

    Created a basic component folder structure, including the components which are Navbar,Welcome,Services,Transactions and Footer. Each component was wrapped in to a single file and a exported together.

--- MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/App.jsx
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/App.jsx
@@ -1,9 +1,16 @@
+import { Navbar, welcome, Footer, Services, Transactions, Welcome } from "./components"
 const App = () => {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+          <Navbar/>
+          <Welcome/>
+        </div>
+          <Services/>
+          <Transactions/>
+          <Footer/>
+      </div>
     </div>
   )
 }
---
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Transactions.jsx
@@ -0,0 +1,10 @@
+//Difference between js and jsx. No diff
+const Transactions = () =>{
+    return (
+        <h1>
+            Transactions
+        </h1>
+    )
+}
+
+export default Transactions;
