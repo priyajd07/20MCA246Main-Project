547993f65557caf6a879d7d42d3807e547d8bbd1
haq9966 <haq9966@gmail.com>
2022-06-01 08:40:02 +0530

    Edited Navbar comp. and added css using Tailwind

--- MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Navbar.jsx
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Navbar.jsx
@@ -1,8 +1,28 @@
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+
+import logo from "../../images/logo.png";
+
+const NavBarItem = ({ title, classprops }) => (
+    <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+  );
+  
 const Navbar = () =>{
     return (
-        <h1>
-            Navbar
-        </h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+          <NavBarItem key={item + index} title={item} />
+        ))}
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          Login
+        </li>
+      </ul>
+        </nav>
     );
 }
 
