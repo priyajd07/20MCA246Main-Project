143dcc3614c6d14ab48ed7b1782997b853e14114
haq9966 <haq9966@gmail.com>
2022-06-04 14:32:11 +0530

    updated the script/ made try catch for main

---
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/smart_contract/scripts/deploy.js
@@ -0,0 +1,21 @@
+const main = async  () => {
+
+  const Greeter = await hre.ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+  await greeter.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+}
+
+const runMain = async () => {
+  try {
+      await main();
+      process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runMain();
