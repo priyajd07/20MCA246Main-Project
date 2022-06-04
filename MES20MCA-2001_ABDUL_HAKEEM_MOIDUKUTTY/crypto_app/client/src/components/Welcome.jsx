15924d27fc2892150492280aee7af55cb4705dc5
haq9966 <haq9966@gmail.com>
2022-06-04 19:03:38 +0530

    fiddling with Transaction context and metamask

--- MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Welcome.jsx
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
+import React, {useContext} from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
+import { TransactionConctext } from "../context/TransactionContext";
 import { Loader } from "./";
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
@@ -18,9 +20,9 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
   );
 
 const Welcome = () => {
-    const connectWallet = () => {
+    const { connectWallet } = useContext(TransactionConctext);
 
-    }
+
     const handleSubmit = () => {
 
     }
@@ -39,7 +41,7 @@ const Welcome = () => {
                         onClick={connectWallet}
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
                     >
-                        <p className="text-white text-base font-semibold">
+                        <p id="connect" className="text-white text-base font-semibold">
                             Connect Wallet
                         </p>
 
@@ -99,6 +101,7 @@ const Welcome = () => {
                   type="button"
                   onClick={handleSubmit}
                   className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                  id="send_now"
                 >
                   Send now
                 </button>
