20fcc7c5dee1dff10f31f0e427cca2fbd9b76f86
haq9966 <haq9966@gmail.com>
2022-06-04 17:30:47 +0530

    utils folder for managin abi and Transactions.json

--- MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/smart_contract/scripts/deploy.js
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/smart_contract/scripts/deploy.js
@@ -3,7 +3,7 @@ const main = async  () => {
   const Transactions = await hre.ethers.getContractFactory("Transactions");
   const transactions = await Transactions.deploy();
 
-  await greeter.deployed();
+  await transactions.deployed();
 
   console.log("Transactions deployed to:", transactions.address);
 }
