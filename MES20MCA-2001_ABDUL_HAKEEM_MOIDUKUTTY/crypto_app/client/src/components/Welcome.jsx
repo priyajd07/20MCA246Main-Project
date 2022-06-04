db4b3ad190e8bafee2d6501ff021b0b34b5f7827
haq9966 <haq9966@gmail.com>
2022-06-04 13:52:22 +0530

    Moving onto implementing Loader component.

--- MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Welcome.jsx
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Welcome.jsx
@@ -5,11 +5,25 @@ import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "./";
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
+
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+
 const Welcome = () => {
     const connectWallet = () => {
 
     }
+    const handleSubmit = () => {
 
+    }
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -69,6 +83,27 @@ const Welcome = () => {
                 </p>
                             </div>
                         </div>
+                    </div>
+                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
+                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
+                    <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
+                    <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+
+                    <div className="h-[1px] w-full bg-gray-400 my-2" />
+                    {false ? (
+                        <Loader />
+                    ) : (
+                        <div>
+                        <button
+                  type="button"
+                  onClick={handleSubmit}
+                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                >
+                  Send now
+                </button>
+                        </div>
+                    )}
                     </div>
                 </div>
             </div>
