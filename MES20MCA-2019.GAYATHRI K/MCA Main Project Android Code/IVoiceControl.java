0aca03a5abb783e3a6d4161796406e6a243eaf92
gayathri916 <74950081+gayathri916@users.noreply.github.com>
2022-06-05 19:16:45 +0530

    Delete IVoiceControl.java

--- MES20MCA-2019.GAYATHRI K/MCA Main Project Android Code/IVoiceControl.java
+++
@@ -1,7 +0,0 @@
-package com.example.talkarena;
-
-public interface IVoiceControl {
-    public abstract void processVoiceCommands(String... voiceCommands); 
-    
-    public void restartListeningService(); 
-}
