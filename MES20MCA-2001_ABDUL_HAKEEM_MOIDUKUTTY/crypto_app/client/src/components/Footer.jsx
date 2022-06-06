fd18d8f8b8ad0952c216b8abda15258edb2e211f
haq9966 <haq9966@gmail.com>
2022-06-06 13:05:07 +0530

    completed the website design

--- MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Footer.jsx
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Footer.jsx
@@ -2,7 +2,27 @@ import logo from '../../images/logo.png'
 const Footer = () =>{
     return (
         <div className='w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer'>
+            <div className='w-ful flex sm:flex-row flex-col justify-between items-center my-4'>
+                <div className='flex flex-0.5 justify-center items-center'>
+                    <img src={logo} alt="logo" className='w-32'/>
+                </div>
+                <div className='flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full'>
+                    <p className='text-white text-base text-center mx-2 cursor-pointer'>Market</p>
+                    <p className='text-white text-base text-center mx-2 cursor-pointer'>Exchange</p>
+                    <p className='text-white text-base text-center mx-2 cursor-pointer'>Tutorials</p>
+                    <p className='text-white text-base text-center mx-2 cursor-pointer'>Wallets</p>
 
+                </div>
+            </div>
+            <div className='flex justify-center items-center flex-col mt-5'>
+                <p className='text-white text-sm text-center'>Come Join Us</p>
+                <p className='text-white text-sm text-center'>info@crypto.com</p>
+            </div>
+            <div className='sm:w-[90%] w-full height-[0.25px] bg-gray-400 mt-5'/>
+            <div className='sm:w-[90%] w-full flex justify-between items-center mt-3'>
+                <p className='text-white text-sm text-center'>@crypto 22 </p>
+                <p className='text-white text-sm text-center'>All rights reserved</p>
+            </div>
         </div>
     );
 }
