91a41d50f3b8b90108a472ccd97986614e5375d4
haq9966 <haq9966@gmail.com>
2022-06-06 11:21:17 +0530

    TransactionContext done**. Moving to Services.jsx

--- MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Services.jsx
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Services.jsx
@@ -1,8 +1,23 @@
+import { BsShieldCheck } from "react-icons/bs";
+import { BiSearchAlt } from 'react-icons/bi';
+import { RiHeart2Fill } from 'react-icons/ri';
+
 const Services = () =>{
     return (
-        <h1>
-            Services
-        </h1>
+        <div className="flex w-full justify-center items-center gradient-bg-services">
+            <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+                <div className="flex-1 flex flex-col justify-start items-start">
+                    <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
+                        Services that we
+                        <br /> 
+                        continue to improve
+                    </h1>
+                </div>
+            </div>
+            <div className="">
+
+            </div>
+        </div>
     );
 }
 
--- MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Welcome.jsx
+++ MES20MCA-2001_ABDUL_HAKEEM_MOIDUKUTTY/crypto_app/client/src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
-import { TransactionConctext } from "../context/TransactionContext";
+import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from "./";
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
@@ -20,11 +20,16 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
   );
 
 const Welcome = () => {
-    const { connectWallet } = useContext(TransactionConctext);
+    const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext);
 
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
+        //to prevent reload
+        e.preventDefault(); 
+        if(!addressTo || !amount || !keyword || !message) return;
 
+        sendTransaction();
     }
     return (
         <div className="flex w-full justify-center items-center">
@@ -36,7 +41,7 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
                     </p>
-                    <button
+                    {!currentAccount && ( <button
                         type="button"
                         onClick={connectWallet}
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
@@ -44,8 +49,9 @@ const Welcome = () => {
                         <p id="connect" className="text-white text-base font-semibold">
                             Connect Wallet
                         </p>
-
                     </button>
+                    )}
+
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
                             Reliabilty
@@ -87,10 +93,11 @@ const Welcome = () => {
                         </div>
                     </div>
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
-                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
-                    <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
-                    <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+                    
+                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+                    <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                    <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
                     <div className="h-[1px] w-full bg-gray-400 my-2" />
                     {false ? (
